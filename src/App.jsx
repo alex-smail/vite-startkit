3f50cdaddfd010a34f665e809c5986602815f7b4
Alex <s8309200@yandex.ru>
2025-01-15 21:14:05 +0400

    no JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,72 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-
 function App() {
-	const [count, setCount] = useState(0); //декларативный
-	const currentDate = new Date().getFullYear(); //декларативный
-	return (
-		<>
-		{/* декларативный - вся разметка HTML */}
-			<div>
-				<a href="https://vite.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img
-						src={reactLogo}
-						className="logo react"
-						alt="React logo"
-					/>
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				{/* императивный */}
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.jsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
-			<br />
-			{/* декларативный */}
-			<p>{currentDate} г.</p>
-		</>
-	);
+  const [count, setCount] = useState(0); //декларативный
+  const currentDate = new Date().getFullYear(); //декларативный
+  return /*#__PURE__*/ React.createElement(
+    React.Fragment,
+    null,
+    /*#__PURE__*/ React.createElement(
+      "div",
+      null,
+      /*#__PURE__*/ React.createElement(
+        "a",
+        {
+          href: "https://vite.dev",
+          target: "_blank"
+        },
+        /*#__PURE__*/ React.createElement("img", {
+          src: viteLogo,
+          className: "logo",
+          alt: "Vite logo"
+        })
+      ),
+      /*#__PURE__*/ React.createElement(
+        "a",
+        {
+          href: "https://react.dev",
+          target: "_blank"
+        },
+        /*#__PURE__*/ React.createElement("img", {
+          src: reactLogo,
+          className: "logo react",
+          alt: "React logo"
+        })
+      )
+    ),
+    /*#__PURE__*/ React.createElement("h1", null, "Vite + React"),
+    /*#__PURE__*/ React.createElement(
+      "div",
+      {
+        className: "card"
+      },
+      /*#__PURE__*/ React.createElement(
+        "button",
+        {
+          onClick: () => setCount((count) => count + 1)
+        },
+        "count is ",
+        count
+      ),
+      /*#__PURE__*/ React.createElement(
+        "p",
+        null,
+        "Edit ",
+        /*#__PURE__*/ React.createElement("code", null, "src/App.jsx"),
+        " and save to test HMR"
+      )
+    ),
+    /*#__PURE__*/ React.createElement(
+      "p",
+      {
+        className: "read-the-docs"
+      },
+      "Click on the Vite and React logos to learn more"
+    ),
+    /*#__PURE__*/ React.createElement("br", null),
+    /*#__PURE__*/ React.createElement("p", null, currentDate, " \u0433.")
+  );
 }
-
 export default App;
